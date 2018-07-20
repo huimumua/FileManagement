b21b34c76a4c535042a367768219817842154374
Harvey <harvey_cheng@askey.com.tw>
2018-07-20 18:55:53 +0800

    修復前一commit不小心移除開檔後關閉 	modified:   app/src/main/jni/cpp/sdcardDefragmentAlg.cpp

--- app/src/main/jni/cpp/sdcardDefragmentAlg.cpp
+++ app/src/main/jni/cpp/sdcardDefragmentAlg.cpp
@@ -240,6 +240,8 @@ int SDA_write_table_in_config(char* mount_path){
     for(i=0; i<TABLE_SIZE; i++){
         fwrite(&FH_Table[i], sizeof(struct file_struct), 1, fp);
     }
+    fclose(fp);
+    return 0;
 }
 
 int SDA_read_table_file_num_from_config(char* mount_path, eFolderType folderType){
