0cced774d80a93fdc0f2bdbe42604b8cb67f9f51
Beck Chen(陈立_Askey_CT) <cl465809880@163.com>
2018-08-31 09:58:50 +0800

    修改sync是用户名的字段类型为String

--- app/src/main/java/com/askey/dvr/cdr7010/filemanagement/askeysettings/AskeySettingSyncTask.java
+++ app/src/main/java/com/askey/dvr/cdr7010/filemanagement/askeysettings/AskeySettingSyncTask.java
@@ -49,7 +49,7 @@ public class AskeySettingSyncTask extends AsyncTask<Void, Integer, Boolean> {
         这个userId的格式必须为"_user1、2、3、4、5"
      */
     private void syncSettings(String userId) {
-        Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_USER_NAME + userId, Settings.Global.getInt(contentResolver, AskeySettings.Global.SYSSET_USER_NAME, 1));
+        Settings.Global.putString(contentResolver, AskeySettings.Global.SYSSET_USER_NAME + userId, Settings.Global.getString(contentResolver, AskeySettings.Global.SYSSET_USER_NAME));
         Settings.Global.putInt(contentResolver, AskeySettings.Global.SYSSET_USER_ID + userId, Settings.Global.getInt(contentResolver, AskeySettings.Global.SYSSET_USER_ID, 1));
         Settings.Global.putInt(contentResolver, AskeySettings.Global.ADAS_FCWS + userId, Settings.Global.getInt(contentResolver, AskeySettings.Global.ADAS_FCWS, 1));
         Settings.Global.putInt(contentResolver, AskeySettings.Global.ADAS_LDS + userId, Settings.Global.getInt(contentResolver, AskeySettings.Global.ADAS_LDS, 1));
